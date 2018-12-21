5fac6560e933297354f7d4b0a810bd5a3bccf65c
Vitor Emanuel Batista <vitorebatista@gmail.com>
2018-12-21 13:02:03 -0200

    remove dist

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 5000;
-app_1.default.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-});
-//# sourceMappingURL=server.js.map
