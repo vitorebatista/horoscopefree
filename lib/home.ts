dc7a4f5c3b3493c6242695518b1b20652c852876
Vitor Emanuel Batista <vitorebatista@gmail.com>
2018-12-24 10:16:57 -0200

    use master md

--- lib/home.ts
+++ lib/home.ts
@@ -4,7 +4,7 @@ import * as showdown from "showdown";
 
 async function home() {
     const converter = new showdown.Converter();
-    return request.get('https://raw.githubusercontent.com/vitorebatista/horoscopefree/dev/README.md')
+    return request.get('https://raw.githubusercontent.com/vitorebatista/horoscopefree/master/README.md')
         .then( ({ text }) => {
             return converter.makeHtml(text);
         })
